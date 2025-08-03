c3dcad5555f1099791282d928431079eec074302
Angel1Curbelo <ecotisat@gmail.com>
2025-08-03 15:41:09 +0000

    Fix: corregida carga de modelos Sequelize

--- server/models/index.js
+++ server/models/index.js
@@ -1,35 +1,37 @@
-
-// server/models/index.js
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+require("dotenv").config();
+
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+  },
+);
 
-// ✅ Cargar modelos modernos con init()
+// Cargar todos los modelos del directorio
 fs.readdirSync(__dirname)
-  .filter(file =>
-    file.indexOf('.') !== 0 &&
-    file !== basename &&
-    file.slice(-3) === '.js'
-  )
-  .forEach(file => {
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
     const modelDefiner = require(path.join(__dirname, file));
-    const ModelClass = modelDefiner(sequelize, Sequelize.DataTypes);
-    db[ModelClass.name] = ModelClass;
+    const model = modelDefiner(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
   });
 
-// ✅ Ejecutar las asociaciones
-Object.keys(db).forEach(modelName => {
+// Asociar modelos si tienen relaciones
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
